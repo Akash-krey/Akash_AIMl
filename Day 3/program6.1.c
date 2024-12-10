93fe2b15f410b20fda2a2f33c9341ac5eed9da1c
Akash-krey <pojaliannaakash@gmail.com>
2024-12-10 09:53:18 +0530

    Add files via upload

---
+++ Day 3/program6.1.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the4 value of num:");
+    scanf("%d",&num);
+    for(int i=1;i<=num;i++)
+    {
+        for(int j=1;j<=num;j++)
+        {
+            printf("%c\t",(64+i));
+        }
+        printf("\n");
+
+    }
+}
